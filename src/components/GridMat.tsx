b35a5feee38373769ddad53bba02aa56c73a6326
EfosaE <osamwonyiefosa02@gmail.com>
2023-10-18 21:11:22 +0100

    Fixed the grid mat distortion

--- src/components/GridMat.tsx
+++ src/components/GridMat.tsx
@@ -5,99 +5,101 @@ import "../index.css";
 const GridMat = () => {
   return (
     <section className='grid-mat-section'>
-      <div className='grid-container'>
+      <div className='grid'>
         <div className='lets-talk'>
           <h3 style={{ fontFamily: "Euclid Galactic", color: "white" }}>
             Let's talk
           </h3>
         </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1-5'></div>
+        <div className='grid-container'>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-4'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-2-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1-5'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-4'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-2-5'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-2-5'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-3'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-3'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-4'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1-5'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bk bk-1-5'></div>
+          </div>
+          <div className='grid-item'>
+            <div className='ellipse pulse bl bl-4'></div>
+          </div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
+          <div className='grid-item'></div>
         </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-4'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-2-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1-5'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-4'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-2-5'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-2-5'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-3'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-3'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-4'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1-5'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-1-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bk bk-1-5'></div>
-        </div>
-        <div className='grid-item'>
-          <div className='ellipse pulse bl bl-4'></div>
-        </div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
-        <div className='grid-item'></div>
       </div>
     </section>
   );
